43288767207df3c2199257c74b6ad6f910579055
giounderrated <giovaniflores162@gmail.com>
2023-02-07 13:54:42 -0600

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { HttpClientModule } from '@angular/common/http';
 import { HolaComponent } from './components/hola/hola.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
--- src/app/components/hola/hola.component.ts
+++ src/app/components/hola/hola.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/core/services/product.service';
 
 @Component({
   selector: 'app-hola',
   templateUrl: './hola.component.html',
   styleUrls: ['./hola.component.scss']
 })
-export class HolaComponent {
+export class HolaComponent implements OnInit{
   
+ products:any[] = [];
+
+  constructor(private productService:ProductService){
+
+  }
+
+  ngOnInit(){
+    this.getProducts();
+  }
+  getProducts() {
+    this.productService.getAllProducts().subscribe((data:any)=>{
+      this.products = data;
+      console.log(data);
+    });
+  }
+
+
+
 }
